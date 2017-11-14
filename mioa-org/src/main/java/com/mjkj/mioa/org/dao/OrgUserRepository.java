a3534a29322cfd33d0d352061b1b7aa49fecb7a6
Administrator <Administrator@192.168.1.100>
2017-11-14 23:24:31 +0800

    用户dao

---
+++ mioa-org/src/main/java/com/mjkj/mioa/org/dao/OrgUserRepository.java
@@ -0,0 +1,31 @@
+/**  
+ * Project Name:mioa-org  
+ * File Name:OrgUserRepository.java  
+ * Package Name:com.mjkj.mioa.org.dao  
+ * Date:2017年11月14日上午11:03:37  
+ * Copyright (c) 2017, 597668255@qq.com All Rights Reserved.  
+ *  
+*/  
+  
+package com.mjkj.mioa.org.dao;  
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.mjkj.mioa.org.entity.TOrgUser;
+
+/**  
+ * ClassName:OrgUserRepository   
+ * Date:     2017年11月14日 上午11:03:37 
+ * @author   fsluo  
+ * @version    
+ * @since    JDK 1.7 
+ * @see        
+ */
+@Repository
+public interface OrgUserRepository extends JpaRepository<TOrgUser, String>
+{
+
+	TOrgUser findByName(String name);
+	
+}
+  
