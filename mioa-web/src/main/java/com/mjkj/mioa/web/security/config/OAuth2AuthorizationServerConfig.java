95af90b6c5196a9a3d9e92ad985338fb859e832a
fsluo <597668255@qq.com>
2017-11-22 09:31:07 +0800

    前端页面提交

--- mioa-web/src/main/java/com/mjkj/mioa/web/security/config/OAuth2AuthorizationServerConfig.java
+++ mioa-web/src/main/java/com/mjkj/mioa/web/security/config/OAuth2AuthorizationServerConfig.java
@@ -10,15 +10,17 @@
 package com.mjkj.mioa.web.security.config;  
 
 import java.util.Arrays;
-
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
@@ -27,7 +29,6 @@ import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
 import com.mjkj.mioa.web.security.service.CustomTokenEnhancer;
 
 /**  
@@ -38,14 +39,19 @@ import com.mjkj.mioa.web.security.service.CustomTokenEnhancer;
  * @since    JDK 1.7 
  * @see        OAuth认证配置类
  */
+@Configuration
+@EnableAutoConfiguration
 public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter
 {
 	@Autowired
     private Environment env;
 	
+	//@Autowired
+    //@Qualifier("authenticationManagerBean")
+    //private AuthenticationManager authenticationManager;
+	
 	@Autowired
-    @Qualifier("authenticationManagerBean")
-    private AuthenticationManager authenticationManager;
+	private mioaAuthenticationProvider authenticationProvider;
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints)
@@ -53,7 +59,16 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
 	{
 		final TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
 		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer()));
-		endpoints.tokenStore(tokenStore()).tokenEnhancer(tokenEnhancerChain).authenticationManager(authenticationManager);
+		endpoints.tokenStore(tokenStore()).tokenEnhancer(tokenEnhancerChain)
+		.authenticationManager(new AuthenticationManager()
+		{
+			
+			@Override
+			public Authentication authenticate(Authentication authentication) throws AuthenticationException
+			{
+				return authenticationProvider.authenticate(authentication);
+			}
+		});
 	}
 	
 	@Override
@@ -76,10 +91,10 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
 	@Bean
     public DataSource dataSource() {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-        dataSource.setUrl(env.getProperty("jdbc.url"));
-        dataSource.setUsername(env.getProperty("jdbc.user"));
-        dataSource.setPassword(env.getProperty("jdbc.pass"));
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driverClassName"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
         return dataSource;
     }
 	
